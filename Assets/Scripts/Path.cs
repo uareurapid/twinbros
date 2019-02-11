48ae92fea0b6288395f1c23c72416e9f3272553b
uareurapid <cristo.paulo@gmail.com>
2019-02-11 22:41:09 +0000

    wip: level 7, stage 2

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+	public Tile[] tiles;
+	public bool showPath = false;
+	public GameObject tileMarker;
+	// Use this for initialization
+	void Start () {
+		if(showPath) {
+			ShowTilesPath();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowTilesPath() {
+		/*foreach(Tile t in tiles) {
+			t.gameObject.AddComponent<>();
+		}*/
+	}
+}
