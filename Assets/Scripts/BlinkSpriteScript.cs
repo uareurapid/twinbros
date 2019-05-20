5f511364b7f72604c691638045187d8b158eba2a
uareurapid <cristo.paulo@gmail.com>
2019-05-20 23:39:29 +0100

    fix tutorial, level 2, stage 3

---
+++ Assets/Scripts/BlinkSpriteScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkSpriteScript : MonoBehaviour {
+
+    public float delay = 0f;
+    public float blinkInterval = 0f;
+   
+	public bool isUIImage = false;
+	private bool canBlink = true;
+	public bool isEnabled = true;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void OnEnable () {
+		InvokeRepeating("Blink",delay,blinkInterval);
+	}
+
+	public void Blink() {
+		isEnabled = !isEnabled;
+		if(isUIImage) {
+			UnityEngine.UI.Image image = GetComponent<UnityEngine.UI.Image>();
+			image.enabled = isEnabled;
+	  	}
+	  	else {
+			SpriteRenderer image = GetComponent<SpriteRenderer>();
+			image.enabled = isEnabled;
+	  	}
+	}
+
+
+}
