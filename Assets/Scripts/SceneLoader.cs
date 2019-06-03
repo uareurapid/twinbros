3c2447a9d6f7512496e2401b5c3a9664bfa37805
PCulo Cristo <pcdreamssoftware@gmail.com>
2019-06-03 21:19:18 +0100

    wip: loading screen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,9 +9,16 @@ public class SceneLoader : MonoBehaviour {
 
 	public string nextScene = "";
 	AsyncOperation asyncOperation;
+
+    //load something automatic instead
+    public bool automaticLoad = false;
+    //used only if above is also set
+    public float delay = 2f;
 	// Use this for initialization
 	void Start () {
-		
+        if(automaticLoad && delay > 0f) {
+            LoadNextSceneNoLevelManager();
+        }
 	}
 	
 	// Update is called once per frame
@@ -27,6 +34,15 @@ public class SceneLoader : MonoBehaviour {
 		}
 		
 	}
+
+    private void LoadNextSceneNoLevelManager()
+    {
+        if (nextScene != null && nextScene != "")
+        {
+            StartCoroutine(LoadSceneNoLevelManager(nextScene));
+        }
+
+    }
 
 	IEnumerator LoadScene(string scene,LevelManager levelManager) {
 
@@ -55,10 +71,50 @@ public class SceneLoader : MonoBehaviour {
 			yield return null;
 		}
 
-
-		levelManager.StartNextStage();
+        if(levelManager!=null) {
+            levelManager.StartNextStage(); 
+        }
+		
 		
 	}
+
+
+    IEnumerator LoadSceneNoLevelManager(string scene)
+    {
+
+        asyncOperation = SceneManager.LoadSceneAsync(nextScene);
+        asyncOperation.allowSceneActivation = false;
+        bool courtinesDone = false;
+
+        foreach (MoveWayPoint point in courtineDoors)
+        {
+            point.stopAfterXPassages = 2;
+            point.justOnce = false;
+            point.ContinueMovement();
+        }
+        while (!asyncOperation.isDone && !courtinesDone)
+        {
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+
+                if (courtineDoors[0].GetNumPassages() == 1 && courtineDoors[1].GetNumPassages() == 1)
+                {
+                    //Activate the Scene
+                    //scene is ready now
+                    courtinesDone = true;
+                }
+
+            }
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(delay);
+        Debug.Log("####################  scene is ready now");
+        asyncOperation.allowSceneActivation = true;
+
+    }
 
 	
 }
